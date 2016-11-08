2a63de8a49ef49a81c04e07dd528c4b9caa71d7f
李　百霊 <anderslee2003@gmail.com>
2016-11-09 00:11:31 +0900

    Revert "test"
    This reverts commit 7ff8fce314beb81b100234f4492c0d4cf5538b93.

--- Euler/src/group1/Problem1.java
+++ Euler/src/group1/Problem1.java
@@ -1,8 +1,6 @@
 package group1;
 
 /**
- * Problem 1
- *
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we
  * get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the
  * multiples of 3 or 5 below 1000.
@@ -11,34 +9,34 @@ package group1;
  */
 public class Problem1 {
 
-    private static final long MAX = 1000;
-    // private static final long MAX = 1000*1000*1000;
+	private static final long MAX = 1000;
+//	private static final long MAX = 1000*1000*1000;
 
-    public static void main(String[] args) {
-        long start = System.nanoTime();
+	public static void main(String[] args) {
+		long start = System.nanoTime();
 
-        long n3 = 0;
-        long n5 = 0;
-        long sum = 0;
+		long n3 = 0;
+		long n5 = 0;
+		long sum = 0;
 
-        while (n3 < MAX || n5 < MAX) {
-            while (n3 <= n5) {
-                n3 += 3;
-                if (n3 < MAX && n3 != n5) {
-                    sum += n3;
-                }
-            }
+		while (n3 < MAX || n5 < MAX) {
+			while (n3 <= n5) {
+				n3 += 3;
+				if (n3 < MAX && n3 != n5) {
+					sum += n3;
+				}
+			}
 
-            while (n5 <= n3) {
-                n5 += 5;
-                if (n5 < MAX && n5 != n3) {
-                    sum += n5;
-                }
-            }
-        }
+			while (n5 <= n3) {
+				n5 += 5;
+				if (n5 < MAX && n5 != n3) {
+					sum += n5;
+				}
+			}
+		}
 
-        System.out.println("Result:" + sum);
-        double elapsedTime = (double) (System.nanoTime() - start) / 1000000;
-        System.out.println("Process Time:" + elapsedTime + "ミリ秒");
-    }
+		System.out.println("Result:" + sum);
+		double elapsedTime = (double) (System.nanoTime() - start) / 1000000;
+		System.out.println("Process Time:" + elapsedTime + "ミリ秒");
+	}
 }
--- bin/README.md
+++
@@ -1,5 +0,0 @@
-# Euler
-Brain training for software engineer ソフトウエアエンジニアの頭脳トレーニング
-
-Project Euler 問題集のオフィシャルサイトは次です。
-https://projecteuler.net/archives
